93e4a0566825ca741ed42df8df74e42db990bf58
Michael Jalkio <mjalkio@gmail.com>
2015-04-28 15:06:21 -0400

    Fixing up Python syntax

--- market.py
+++ market.py
@@ -1,28 +1,33 @@
 import random
 
+
 class DiningLocation:
-    base_time_range = range(10,30)
+
     def __init__(self, index):
         self.index = index
-        self.base_time = random.choice(base_time_range)
+        self.base_time = random.choice(range(10, 30))
         self.num_students = 0
 
-    def serve_student(s):
+    def serve_student(self, s):
         self.num_students += 1
 
-    def serving_time():
+    def serving_time(self):
         return self.base_time + self.num_students
 
-    def max_students(max_time):
+    def max_students(self, max_time):
         return max_time - self.base_time
 
-    def full(max_time):
-        return max_students(max_time) >= self.num_students
+    def full(self, max_time):
+        return self.max_students(max_time) >= self.num_students
+
 
 class Student:
-    __init__(self, index, locations):
+
+    def __init__(self, index, locations):
         self.index = index
         self.pref_list = random.shuffle(locations)
 
-locations = [ DiningLocation(i) for i in range(m)]
+m = 10
+n = 100
+locations = [DiningLocation(i) for i in range(m)]
 students = [Student(i, locations) for i in range(n)]
