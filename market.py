7db2d8397bbb5ab8bfc90a0bbcb676943854a596
Rachel <rae84@cornell.edu>
2015-04-28 23:41:40 -0400

    implement simple serial dictatorship

--- market.py
+++ market.py
@@ -3,30 +3,36 @@ import random
 
 class DiningLocation:
 
-    def __init__(self, index):
+    def __init__(self, index, max_time):
         self.index = index
         self.base_time = random.choice(range(10, 30))
         self.num_students = 0
+        self.max_time = max_time
 
     def serve_student(self, s):
-        self.num_students += 1
-        s.location = self
+        if not self.full():
+            self.num_students += 1
+            s.location = self
+            return True
+        else:
+            return False
 
     def serving_time(self):
         return self.base_time + self.num_students
 
-    def max_students(self, max_time):
-        return max_time - self.base_time
+    def max_students(self):
+        return self.max_time - self.base_time
 
-    def full(self, max_time):
-        return self.max_students(max_time) >= self.num_students
+    def full(self):
+        return self.num_students >= self.max_students()
 
 
 class Student:
 
     def __init__(self, index, locations, incomplete_prefs):
         self.index = index
-        self.pref_list = random.shuffle(locations)
+        self.pref_list = [location.index for location in locations]
+        random.shuffle(self.pref_list)
         self.location = None
         if incomplete_prefs:
             list_size = random.uniform(1, len(locations))
@@ -35,17 +41,18 @@ class Student:
     def happiness(self, m):
         if self.location is None:
             return 0
-        if self.location not in self.pref_list:
+        if self.location.index not in self.pref_list:
             return -1
-        location_index = self.pref_list.index(self.location)
+        location_index = self.pref_list.index(self.location.index)
         return float(m - location_index) / m
 
 
 class Market:
-    def __init__(self, n, m, incomplete_prefs=False):
-        self.locations = [DiningLocation(i) for i in range(m)]
-        self.students = [Student(i, self.locations, incompete_prefs) for i in range(n)]
-
+    def __init__(self, n, m, max_time, incomplete_prefs=False):
+        self.locations = [DiningLocation(i, max_time) for i in range(m)]
+        self.students = [Student(i, self.locations, incomplete_prefs) for i in range(n)]
+        self.max_time = max_time
 m = 10
 n = 100
-market = Market(n, m)
+max_time = 45
+market = Market(n, m, max_time)
---
+++ serialDictatorship.py
@@ -0,0 +1,18 @@
+from market import Market, Student
+
+def serial_dictatorship(market):
+    for student in market.students:
+        for location_index in student.pref_list:
+            if market.locations[location_index].serve_student(student):
+                break
+
+market = Market(100, 10, 35, False)
+serial_dictatorship(market)
+
+max_students = [loc.max_students() for loc in market.locations]
+print max_students
+print sum(max_students)
+
+happy = [student.happiness(10) for student in market.students]
+print happy
+print sum(happy)
