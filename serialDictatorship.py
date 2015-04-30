e8189dfe791cfe24093b76f08088e606abda468c
Rachel <rae84@cornell.edu>
2015-04-29 22:15:14 -0400

    add modified serial dictatorship algorithm to optimize on happiness

--- serialDictatorship.py
+++ serialDictatorship.py
@@ -1,19 +1,18 @@
 from market import Market
 
-
 def serial_dictatorship(market):
     for student in market.students:
         for location_index in student.pref_list:
             if market.locations[location_index].serve_student(student):
                 break
 
-market = Market(100, 10, 35, False)
-serial_dictatorship(market)
+def modified_serial_dictatorship(market):
+    max_students_served = sum([location.max_students() for location in market.locations])
+    min_unserved = len(market.students) - max_students_served
+    n = 0
+    unserved_students = [student for student in market.students if student.location is None]
+    while len(unserved_students) > min_unserved and n < len(market.locations) - 1:
+        unserved_students = [student for student in unserved_students if not market.locations[student.pref_list[n]].serve_student(student)]
+        n += 1
 
-max_students = [loc.max_students() for loc in market.locations]
-print max_students
-print sum(max_students)
 
-happy = [student.happiness(10) for student in market.students]
-print happy
-print sum(happy)
