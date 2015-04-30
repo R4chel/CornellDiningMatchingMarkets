70822158e298d33e3da996d6010c17e1f32ed091
Rachel <rae84@cornell.edu>
2015-04-29 21:28:02 -0400

    add evaluation file

---
+++ evaluation.py
@@ -0,0 +1,20 @@
+from market import Market
+
+def happy(market):
+    m = len(market.locations)
+    happy = [student.happiness(m) for student in market.students]
+    return sum(happy)
+
+def percent_served(market):
+    num_served = sum([1 for student in market.students if student.location is not None])
+    return float(num_served)/len(market.students)
+
+def total_time(market):
+    serving_time = [student.location.serving_time() for student in market.students if student.location is not None]
+    return sum(serving_time)
+
+def average_time(market):
+    num_served = sum([1 for student in market.students if student.location is not None])
+    return float(total_time(market))/num_served
+
+
