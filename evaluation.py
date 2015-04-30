f1d97aa5019d8e2adb39892ac0e8c3a27837b3b9
Michael Jalkio <mjalkio@gmail.com>
2015-04-29 22:48:38 -0400

    Fix syntax

--- evaluation.py
+++ evaluation.py
@@ -1,22 +1,27 @@
 from market import Market
 
+
 def happy(market):
     m = len(market.locations)
     happy = [student.happiness(m) for student in market.students]
     return sum(happy)
 
+
 def percent_served(market):
-    num_served = sum([1 for student in market.students if student.location is not None])
-    return float(num_served)/len(market.students)
+    num_served = sum([1 for student in market.students
+                      if student.location is not None])
+    return float(num_served) / len(market.students)
+
 
 def total_time(market):
-    serving_time = [student.location.serving_time() for student in market.students if student.location is not None]
+    serving_time = [student.location.serving_time() for student
+                    in market.students if student.location is not None]
     return sum(serving_time)
 
+
 def average_time(market):
-    num_served = sum([1 for student in market.students if student.location is not None])
-    if num_served = 0:
+    num_served = sum([1 for student in market.students
+                      if student.location is not None])
+    if num_served == 0:
         return 0
-    return float(total_time(market))/num_served
-
-
+    return float(total_time(market)) / num_served
